c8a2617d0dee1e7db9151191c2c582fb2d790fe9
bottycle <clement.botty@etu.utc.fr>
2021-11-22 17:11:18 +0100

    feat(LoginComponents) : created first draft of login component

--- src/Components/Authentication/Login/Login.js
+++ src/Components/Authentication/Login/Login.js
@@ -1,10 +1,21 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
+import {Container, Stack, TextField} from '@mui/material';
+import { LoginButton } from '../LoginButton';
 
 const LoginComponent = () => {
 
     return (
-        <Button variant="contained">Hello World</Button>
+      <Container maxWidth="sm">
+
+        <Stack spacing={3} mt={4}>
+          <TextField id="outlined-basic" label="Email" variant="outlined" type="email"/>
+          <TextField id="outlined-basic" label="Password" variant="outlined" type="password" />
+          <LoginButton></LoginButton>
+        </Stack>
+
+
+      </Container>
+        
     )
 }
 
---
+++ src/Components/Authentication/LoginButton.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+const LoginButton = () => {
+
+    return (
+        <Button variant="contained">Login</Button>
+    )
+}
+
+export {LoginButton}
