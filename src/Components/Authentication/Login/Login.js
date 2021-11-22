3b62758ca9a7d69907a05155dbc5e053909fcdc6
Maylis <maylisdetalhouet@gmail.com>
2021-11-22 16:10:23 +0100

    first component

---
+++ src/Components/Authentication/Login/Login.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+const LoginComponent = () => {
+
+    return (
+        <Button variant="contained">Hello World</Button>
+    )
+}
+
+export {LoginComponent}
