83ec36b05b8f5a1c8b7fb891d12136ecdbb42465
Maylis <maylisdetalhouet@gmail.com>
2021-11-25 12:02:42 +0100

    User management table

--- src/Components/Admin/CreateUser.js
+++ src/Components/Admin/CreateUser.js
@@ -4,15 +4,15 @@ import { Container, Stack, TextField, Button } from '@mui/material';
 const CreateUser = () => {
     return (
         <Container maxWidth="sm">
-            <Stack spacing={3} mt={4}>
-                <TextField id="outlined-basic" label="Firstname" variant="outlined" />
-                <TextField id="outlined-basic" label="Lastname" variant="outlined" />
-                <TextField id="outlined-basic" label="Email" variant="outlined" type="email" />
-                <TextField id="outlined-basic" label="Phone Number" variant="outlined" />
-                <TextField id="outlined-basic" label="Company" variant="outlined" />
+          <Stack spacing={3} mt={4}>
+            <TextField id="outlined-basic" label="Firstname" variant="outlined" />
+            <TextField id="outlined-basic" label="Lastname" variant="outlined" />
+            <TextField id="outlined-basic" label="Email" variant="outlined" type="email" />
+            <TextField id="outlined-basic" label="Phone Number" variant="outlined" />
+            <TextField id="outlined-basic" label="Company" variant="outlined" />
 
-                <Button variant="outlined">Create Profile</Button>
-            </Stack>
+            <Button variant="outlined">Create Profile</Button>
+          </Stack>
         </Container>
     );
 };
--- src/Components/Admin/UserManagement.js
+++ src/Components/Admin/UserManagement.js
@@ -2,17 +2,54 @@ import * as React from 'react';
 import { Button, Grid } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import { LogoutButton } from '../Authentication/LogoutButton';
+import MUIDataTable from "mui-datatables";
+import { useHistory } from 'react-router-dom'
+
 
 const UserManagement = () => {
+  const history = useHistory()
+  const columns = [
+    { 
+      name: "First Name"
+    },
+    {
+      name: "Last Name"
+    },
+    {
+      name:"Company"
+    },
+    {
+      name: "Action", 
+      options:{ 
+        customBodyRender: (value, tableMeta, updateValue) => {
+        return (
+          <Button>Action Button</Button>
+        );
+        }
+      }
+    }
+  ];
+
+    const data = [
+        ["Michel", "Dupont", "Les bronzés font du ski",],
+        ["Pierre", "Durant", "UTC"],
+       ];
     return (
-      <>
+      <>      
         <Grid container direction="row" justifyContent="center" alignItems="center">
-          <Stack direction="column" spacing={2} mt={2}>
-            <Button variant="contained" href="/admin/create-user">
-                {' '}
-              New trainee{' '}
-            </Button>
+          <Stack direction="column" spacing={2} mt={2} alignItems={'flex-end'}>
+            <Stack direction="column" alignItems={'flex-end'} spacing={2}>
             <LogoutButton />
+              <Button variant="contained" onClick={() => {history.push('/admin/create-user')}}>
+                  {' '}
+                New trainee{' '}
+              </Button>
+            </Stack>
+            <MUIDataTable
+              title={"Trainees"}
+              data={data}
+              columns={columns}
+            />
           </Stack>
         </Grid>
       </>
