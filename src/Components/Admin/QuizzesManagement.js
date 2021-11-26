f953815de924ee13b0665817db83b1794b98fc03
Maylis <maylisdetalhouet@gmail.com>
2021-11-26 17:37:30 +0100

    Admin view: quizzes management

---
+++ src/Components/Admin/QuizzesManagement.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { Button, Grid } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import { LogoutButton } from '../Authentication/LogoutButton';
+import MUIDataTable from "mui-datatables";
+import { useHistory } from 'react-router-dom'
+
+
+const QuizzesManagement = () => {
+  const history = useHistory()
+  const columns = [
+    { 
+      name: "Quizz"
+    },
+    {
+      name: "Theme"
+    },
+    {
+      name:"Number of records"
+    },
+    {
+      name: "Edit quizz", 
+      options:{ 
+        customBodyRender: (value, tableMeta, updateValue) => {
+        return (
+          <Button>Edit</Button>
+        );
+        }
+      }
+    }
+  ];
+
+    const data = [
+        ["Intro JEE", "Back", "8"],
+        ["Intro React", "Front", "57"],
+       ];
+    return (
+      <>      
+        <Grid container direction="row" justifyContent="center" alignItems="center">
+          <Stack direction="column" spacing={2} mt={2} alignItems={'flex-end'}>
+            <Stack direction="column" alignItems={'flex-end'} spacing={2}>
+            <LogoutButton />
+              <Button variant="contained" onClick={() => {history.push('/admin/create-user')}}>
+                New quizz
+              </Button>
+            </Stack>
+            <Stack spacing={8} bottom={2}>
+              <MUIDataTable
+                title={"Active quizzes"}
+                data={data}
+                columns={columns}
+              />
+              <MUIDataTable
+                title={"Inactive quizzes"}
+                data={data}
+                columns={columns}
+              />
+            </Stack>
+          </Stack>
+        </Grid>
+      </>
+    );
+};
+
+export { QuizzesManagement };
--- src/Components/WelcomePage/Admin/AdminComponent.js
+++ src/Components/WelcomePage/Admin/AdminComponent.js
@@ -2,16 +2,20 @@ import * as React from 'react';
 import { Button, Grid } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import { LogoutButton } from '../../Authentication/LogoutButton';
+import { useHistory } from 'react-router-dom'
 
 const AdminComponent = () => {
+  const history = useHistory()
+
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Stack direction="column" spacing={2}  mt={2}>
-                    <Button variant="contained" href="/admin/user-management">
+                    <Button variant="contained" onClick={() => {history.push('/admin/user-management')}}>
                         User Management
                     </Button>
-                    <Button variant="contained">Quizzes Management</Button>
+                    <Button variant="contained" onClick={() => {history.push('/admin/quizzes-management')}}>
+                      Quizzes Management</Button>
                     <LogoutButton />
                 </Stack>
             </Grid>
