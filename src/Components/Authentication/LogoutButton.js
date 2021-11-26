4eb5c76225392363c076b53c2171658d272dbd44
Maylis <maylisdetalhouet@gmail.com>
2021-11-26 17:09:46 +0100

    improved logout button

--- src/Components/Authentication/LogoutButton.js
+++ src/Components/Authentication/LogoutButton.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import { useHistory } from 'react-router-dom'
 
 const LogoutButton = () => {
+  const history = useHistory()
     return (
-        <Button variant="outlined" href="/login">
+        <Button variant="outlined" onClick={() => {history.push('/login')}}>
             Logout
         </Button>
     );
